2a084b7344d7b937424718092bed5b02a8a16879
Jubayer husain <jubayerhusai360@gmail.com>
2024-09-19 20:20:43 +0600

    add money done with statment

--- script/home.js
+++ script/home.js
@@ -1,1 +1,21 @@
-console.log('hey jubayer your payoo home page done')
+document.getElementById("addmoney-btn").addEventListener('click', function (event) {
+    event.preventDefault();
+    const addAmount = document.getElementById('amount').value;
+    const addAmountfloat= parseFloat(addAmount) ;
+    // console.log(typeof addAmountfloat);
+    // console.log(addAmountfloat);
+    const pinNumber = document.getElementById('pin-number').value;
+    if(pinNumber === '1234'){
+        // console.log("hey Juabyer your money added done")
+        const currentAmount = document.getElementById('current-balance').innerText;
+        const currentAmountFloat = parseFloat(currentAmount)
+        // console.log(typeof currentAmountFloat)
+        // console.log(currentAmountFloat)
+        const totalBalance = currentAmountFloat + addAmountfloat;
+        // console.log(totalBalance)
+        document.getElementById('current-balance').innerText = totalBalance;
+    }
+    else{
+        alert('Some things wrong please try again')
+    }
+})
